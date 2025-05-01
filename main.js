32043b35951d2573f4a1304b5a8fe1efe03f8dab
thurler142 <thurler9@gmail.com>
2025-05-01 16:44:55 -0300

    .

--- main.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('changeColorBtn').addEventListener('click', function() {
-    const colors = ['#f0f0f0', '#ffcccb', '#d1ffcc', '#ccd1ff', '#ffeccc'];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.body.style.backgroundColor = randomColor;
-});
