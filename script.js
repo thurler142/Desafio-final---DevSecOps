fd6743df1fe9cb92a82f1302522a2bf32d904e1d
thurler142 <thurler9@gmail.com>
2025-05-01 17:52:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.getElementById('changeColorBtn').addEventListener('click', function() {
-    musicaVasco.play();
     const colors = ['#f0f0f0', '#ffcccb', '#d1ffcc', '#ccd1ff', '#ffeccc'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     document.body.style.backgroundColor = randomColor;
