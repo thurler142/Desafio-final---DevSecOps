9b0e263ee792579632477d05329fac80fc161ab3
thurler142 <thurler9@gmail.com>
2025-05-01 17:44:05 -0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-document.getElementById('changeColorBtn').addEventListener('click', function() {
+document.getElementById('VascoBtn').addEventListener('click', function() {
+    musicaVasco.play()
     const colors = ['#f0f0f0', '#ffcccb', '#d1ffcc', '#ccd1ff', '#ffeccc'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     document.body.style.backgroundColor = randomColor;
